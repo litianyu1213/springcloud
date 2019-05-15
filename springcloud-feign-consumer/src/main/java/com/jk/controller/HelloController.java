149322b9affcc9ded7e26d6aefda4435e08b1f60
litianyu <942508604@qq.com>
2019-05-15 17:38:13 +0800

    提交文件

--- springcloud-feign-consumer/src/main/java/com/jk/controller/HelloController.java
+++ springcloud-feign-consumer/src/main/java/com/jk/controller/HelloController.java
@@ -28,6 +28,7 @@ public class HelloController {
         user.setId(1);
         user.setName("阿萨德撒");
         user.setSex("男");
+        System.out.println(6666);
         return schedualServiceHi.HelloUser( user );
     }
 
